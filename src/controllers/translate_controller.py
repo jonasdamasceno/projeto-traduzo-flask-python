5b8a4ef2dc84c0120f8ae7e76150658f821a56b6
jonas <jonasmichael2021@outlook.com>
2024-05-16 13:48:28 -0300

    requisito 6

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint("translate_controller", __name__)
 
 
-@translate_controller.route("/", methods=["GET"])
+@translate_controller.route("/", methods=["GET", "POST"])
 def index():
     # Obter todos os idiomas
     all_languages = LanguageModel.list_dicts()
@@ -18,5 +19,22 @@ def index():
         "translated": "What do you want to translate?",
     }
 
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=all_languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
     # Renderizar o template com os dados padrão
     return render_template("index.html", **default_data)
