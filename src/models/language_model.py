0aa3ac9eaa6285eb62d589057076d7892c1ed2b8
jonas <jonasmichael2021@outlook.com>
2024-05-16 09:14:32 -0300

    requisito 2

---
+++ src/models/language_model.py
@@ -0,0 +1,15 @@
+from .abstract_model import AbstractModel
+from database.db import db
+
+
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def to_dict(self):
+        return {
+            "name": self.data["name"],
+            "acronym": self.data["acronym"],
+        }
