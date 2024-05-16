b3ed5c663255988fff2a428cf0a94f2d3795a90c
jonas <jonasmichael2021@outlook.com>
2024-05-16 16:36:40 -0300

    requisito 7

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -38,3 +38,37 @@ def index():
         )
     # Renderizar o template com os dados padrão
     return render_template("index.html", **default_data)
+
+
+def get_request_parameters():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    return text_to_translate, translate_from, translate_to
+
+
+def translate_text(text_to_translate, translate_from, translate_to):
+    translated_text = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+    return translated_text
+
+
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse():
+    text_to_translate, translate_from, translate_to = get_request_parameters()
+
+    translated_text = translate_text(
+        text_to_translate, translate_from, translate_to
+    )
+
+    languages = LanguageModel.list_dicts()
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=translated_text,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
