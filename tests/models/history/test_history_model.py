fed12e1d0c87b1601caec1520d118abc5b0ef076
jonas <jonasmichael2021@outlook.com>
2024-05-17 09:53:53 -0300

    requisito 8

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,25 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    expected_history = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+
+    history = json.loads(HistoryModel.list_as_json())
+
+    for item in history:
+        del item["_id"]
+
+    assert history == expected_history
